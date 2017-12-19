87dcea3d19a6b9a789a7110035d126622ece48c8
Librazy <im@librazy.org>
2017-12-19 20:04:37 +0800

    init

---
+++ Controllers/Login/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xmu.Crms.Web.Group1.Controllers.Login
+{
+    [Route("")]
+    public class LoginController : Controller
+    {
+        [Route("/")]
+        public IActionResult HomePage()
+        {
+            return Redirect("/Login");
+        }
+
+        [Route("/Login")]
+        public IActionResult AccountLoginPage()
+        {
+            return View();
+        }
+
+        [Route("/RegisterPage")]
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xmu.Crms.Web.Group1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
